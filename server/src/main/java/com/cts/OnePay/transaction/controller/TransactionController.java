c3e9ad48f4e324cfb5df3506d35e676829a2959b
Atif Ali <atif.ali2@cognizant.com>
2026-04-20 16:52:01 +0530

    Transaction: Added controller endpoints for transaction services

--- server/src/main/java/com/cts/OnePay/transaction/controller/TransactionController.java
+++ server/src/main/java/com/cts/OnePay/transaction/controller/TransactionController.java
@@ -4,12 +4,10 @@ import com.cts.OnePay.transaction.dto.TransactionResponseDto;
 import com.cts.OnePay.transaction.dto.TransferRequestDto;
 import com.cts.OnePay.transaction.service.TransactionService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/transaction")
@@ -23,5 +21,28 @@ public class TransactionController {
         Long userId= 1L;
         TransactionResponseDto responseDto= transactionService.transferMoney(userId,requestDto);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<TransactionResponseDto>>  getMyTransactions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        // TODO: Extract userId from security context
+        Long userId= 1L;
+        Page<TransactionResponseDto> transactions= transactionService.getMyTransactions(userId,page,size);
+        return ResponseEntity.ok(transactions);
+    }
+
+    @GetMapping("/{transactionId}")
+    public ResponseEntity<TransactionResponseDto>  getMyTransactions(@PathVariable Long transactionId){
+        // TODO: Extract userId from security context
+        Long userId= 1L;
+        TransactionResponseDto transaction= transactionService.getMyTransactionById(transactionId,userId);
+        return ResponseEntity.ok(transaction);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<TransactionResponseDto>>  getAllTransactions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        // TODO: Make sure only admin can access
+        Page<TransactionResponseDto> transactions= transactionService.getAllTransactions(page,size);
+        return ResponseEntity.ok(transactions);
     }
 }
