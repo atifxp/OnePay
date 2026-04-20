edfbeed860035515130e8988602ee4733a11c163
Atif Ali <atif.ali2@cognizant.com>
2026-04-20 11:54:23 +0530

    Transaction: added ledger method to handle ledger entry

--- server/src/main/java/com/cts/OnePay/transaction/service/LedgerService.java
+++ server/src/main/java/com/cts/OnePay/transaction/service/LedgerService.java
@@ -1,10 +1,17 @@
 package com.cts.OnePay.transaction.service;
 
 import com.cts.OnePay.transaction.dto.LedgerDto;
+import com.cts.OnePay.transaction.model.Transaction;
+import com.cts.OnePay.transaction.model.Wallet;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface LedgerService {
     public List<LedgerDto> getMyPassbook(Long userId);
     public LedgerDto getLedgerById(Long ledgerId);
+
+    void record(Wallet senderWallet, Wallet receiverWallet, Transaction transaction, @DecimalMin(value = "1.0", message = "Transaction amount must be at least 1") BigDecimal amount, BigDecimal senderBalanceBefore, @Min(value = 0, message = "Balance cannot be less than 0") BigDecimal balance, BigDecimal receiverBalanceBefore, @Min(value = 0, message = "Balance cannot be less than 0") BigDecimal balance1);
 }
--- server/src/main/java/com/cts/OnePay/transaction/service/LedgerServiceImpl.java
+++ server/src/main/java/com/cts/OnePay/transaction/service/LedgerServiceImpl.java
@@ -1,13 +1,21 @@
 package com.cts.OnePay.transaction.service;
 
 import com.cts.OnePay.transaction.dto.LedgerDto;
+import com.cts.OnePay.transaction.model.Transaction;
+import com.cts.OnePay.transaction.model.Wallet;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LedgerServiceImpl implements LedgerService {
+    @Override
+    public void record(Wallet senderWallet, Wallet receiverWallet, Transaction transaction, BigDecimal amount, BigDecimal senderBalanceBefore, BigDecimal balance, BigDecimal receiverBalanceBefore, BigDecimal balance1) {
+
+    }
+
     public List<LedgerDto> getMyPassbook(Long userId){
         ArrayList<LedgerDto> ledgers= new ArrayList<>();
         return ledgers;
