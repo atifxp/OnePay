7ff11e30a0cdd5c51418004b078c60919a8314dd
Atif Ali <atif.ali2@cognizant.com>
2026-04-20 18:03:30 +0530

    Transaction: Add Ledger record service function

--- server/src/main/java/com/cts/OnePay/transaction/service/LedgerServiceImpl.java
+++ server/src/main/java/com/cts/OnePay/transaction/service/LedgerServiceImpl.java
@@ -1,8 +1,12 @@
 package com.cts.OnePay.transaction.service;
 
 import com.cts.OnePay.transaction.dto.LedgerDto;
+import com.cts.OnePay.transaction.model.Ledger;
 import com.cts.OnePay.transaction.model.Transaction;
 import com.cts.OnePay.transaction.model.Wallet;
+import com.cts.OnePay.transaction.model.enums.EntryType;
+import com.cts.OnePay.transaction.repository.LedgerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -10,10 +14,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class LedgerServiceImpl implements LedgerService {
-    @Override
-    public void record(Wallet senderWallet, Wallet receiverWallet, Transaction transaction, BigDecimal amount, BigDecimal senderBalanceBefore, BigDecimal balance, BigDecimal receiverBalanceBefore, BigDecimal balance1) {
 
+    private final LedgerRepository ledgerRepository;
+
+    public void record(Wallet senderWallet, Wallet receiverWallet, Transaction transaction, BigDecimal amount, BigDecimal senderBefore,   BigDecimal senderAfter,
+                       BigDecimal receiverBefore, BigDecimal receiverAfter) {
+        Ledger credit= new Ledger();
+        credit.setAmount(amount);
+        credit.setWallet(receiverWallet);
+        credit.setTransaction(transaction);
+        credit.setEntryType(EntryType.CREDIT);
+        credit.setBalanceBefore(receiverBefore);
+        credit.setBalanceAfter(receiverAfter);
+
+        Ledger debit= new Ledger();
+        credit.setAmount(amount);
+        credit.setWallet(senderWallet);
+        credit.setTransaction(transaction);
+        credit.setEntryType(EntryType.DEBIT);
+        credit.setBalanceBefore(senderBefore);
+        credit.setBalanceAfter(senderAfter);
+
+        ledgerRepository.save(credit);
+        ledgerRepository.save(debit);
     }
 
     public List<LedgerDto> getMyPassbook(Long userId){
