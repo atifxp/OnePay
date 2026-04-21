657046e5b2d216f88e94701e37357351a8d163bb
sayanpaul-1702 <Sayan.Paul4@cognizant.com>
2026-04-21 09:56:46 +0530

    Implemented UserDetailsService, UserDetails

--- server/src/main/java/com/cts/OnePay/user/model/MyUserDetails.java
+++ server/src/main/java/com/cts/OnePay/user/model/MyUserDetails.java
@@ -1,26 +1,35 @@
 package com.cts.OnePay.user.model;
 
+import lombok.AllArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-
+@AllArgsConstructor
 public class MyUserDetails implements UserDetails {
+
+    private final User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.user.getRole()));
     }
 
     @Override
     public @Nullable String getPassword() {
-        return "";
+        return this.user.getPasswordHash();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return this.user.getEmail();
+    }
+
+    public Long getUserId(){
+        return this.user.getUserId();
     }
 }
--- server/src/main/java/com/cts/OnePay/user/service/MyUserDetailsService.java
+++ server/src/main/java/com/cts/OnePay/user/service/MyUserDetailsService.java
@@ -1,14 +1,27 @@
 package com.cts.OnePay.user.service;
 
+import com.cts.OnePay.user.model.MyUserDetails;
+import com.cts.OnePay.user.model.User;
+import com.cts.OnePay.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new AuthenticationCredentialsNotFoundException("User not found"));
+
+        return new MyUserDetails(user);
     }
 }
