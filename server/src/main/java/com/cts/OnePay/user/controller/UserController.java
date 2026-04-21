fd1eb84d7e6d5827a6c6677dccd793ac5b6039d0
sayanpaul-1702 <Sayan.Paul4@cognizant.com>
2026-04-21 15:59:13 +0530

    modified UserController and VerificationController to accomodate security features

--- server/src/main/java/com/cts/OnePay/user/controller/UserController.java
+++ server/src/main/java/com/cts/OnePay/user/controller/UserController.java
@@ -2,10 +2,12 @@ package com.cts.OnePay.user.controller;
 
 import com.cts.OnePay.user.dto.userDtos.UserResponseDto;
 import com.cts.OnePay.user.dto.userDtos.UserUpdateRequestDto;
+import com.cts.OnePay.user.model.MyUserDetails;
 import com.cts.OnePay.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,15 +18,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/me")
-    public ResponseEntity<UserResponseDto> getProfile(){
-        Long userId = 1L; //Placeholder: To be changed
+    public ResponseEntity<UserResponseDto> getProfile(@AuthenticationPrincipal MyUserDetails userDetails){
+        Long userId = userDetails.getUserId();
         UserResponseDto response = userService.getProfile(userId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<?> updateProfile(@RequestBody UserUpdateRequestDto dto){
-        Long userId = 1L; //Placeholder: To be changed
+    public ResponseEntity<?> updateProfile(@RequestBody UserUpdateRequestDto dto, @AuthenticationPrincipal MyUserDetails userDetails){
+        Long userId = userDetails.getUserId();
         UserResponseDto response = userService.updateProfile(userId,dto);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
--- server/src/main/java/com/cts/OnePay/user/controller/VerificationController.java
+++ server/src/main/java/com/cts/OnePay/user/controller/VerificationController.java
@@ -3,12 +3,15 @@ package com.cts.OnePay.user.controller;
 import com.cts.OnePay.user.dto.verificatioDtos.VerificationPendingResponseDto;
 import com.cts.OnePay.user.dto.verificatioDtos.VerificationRequestDto;
 import com.cts.OnePay.user.dto.verificatioDtos.VerificationUpdateRequestDto;
+import com.cts.OnePay.user.model.MyUserDetails;
 import com.cts.OnePay.user.service.VerificationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -21,16 +24,16 @@ public class VerificationController {
     private VerificationService verificationService;
 
     @PostMapping("/submit")
-    public ResponseEntity<Map<String,String>> submit(@RequestBody @Valid VerificationRequestDto verificationRequest){
-        Long userId = 1L; //Placeholder: To be Changed
+    public ResponseEntity<Map<String,String>> submit(@RequestBody @Valid VerificationRequestDto verificationRequest, @AuthenticationPrincipal MyUserDetails userDetails){
+        Long userId = userDetails.getUserId();
         Map<String, String> response = verificationService.submitDoc(userId,verificationRequest);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/status")
-    public ResponseEntity<Map<String,String>> checkStatus(){
-        Long userId = 1L;
+    public ResponseEntity<Map<String,String>> checkStatus(@AuthenticationPrincipal MyUserDetails userDetails){
+        Long userId = userDetails.getUserId();
         Map<String, String> response = verificationService.checkStatus(userId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
@@ -38,20 +41,23 @@ public class VerificationController {
     //ADMIN routes
 
     @PatchMapping("/{userId}/approve")
-    public ResponseEntity<Map<String, String>> approve(@PathVariable("userId")Long userId, @RequestBody VerificationUpdateRequestDto updates){
-        Long verifierId = 1L;
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, String>> approve(@PathVariable("userId")Long userId, @RequestBody VerificationUpdateRequestDto updates, @AuthenticationPrincipal MyUserDetails userDetails){
+        Long verifierId = userDetails.getUserId();
         Map<String, String> response = verificationService.approve(verifierId, userId, updates);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PatchMapping("/{userId}/reject")
-    public ResponseEntity<Map<String, String>> reject(@PathVariable("userId")Long userId, @RequestBody VerificationUpdateRequestDto updates){
-        Long verifierId = 1L;
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, String>> reject(@PathVariable("userId")Long userId, @RequestBody VerificationUpdateRequestDto updates, @AuthenticationPrincipal MyUserDetails userDetails){
+        Long verifierId = userDetails.getUserId();
         Map<String, String> response = verificationService.reject(verifierId, userId, updates);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/pending/{pageNo}/{pageSize}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Page<VerificationPendingResponseDto>> fetchPendingVerificationRequest(
             @PathVariable("pageNo")int pageNo,
             @PathVariable("pageSize") int pageSize
